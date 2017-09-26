740d3f1474131d7f265ff8bacedaaae7319630f5
nikolaysalinder <nikolay.salinder@gmail.com>
2017-09-26 06:24:01 +0500

    Finished chapter 10

--- credentials.js
+++ credentials.js
@@ -1,3 +1,7 @@
 module.exports = {
-  cookieSecret: 'Здесь находится ва секрет cookie-файла'
+  cookieSecret: 'Здесь находится ва секрет cookie-файла',
+  gmail: {
+    user: 'meadowlarktravel1@gmail.com',
+    password: 'Vasiliy88'
+  }
 }
--- index.js
+++ index.js
@@ -1,21 +1,54 @@
 var express = require('express');
-var fortune = require('./lib/fortune.js');
 var formidable = require('formidable');
 var jqupload = require('jquery-file-upload-middleware');
+var nodemailer = require('nodemailer');
+
 
 var app = express();
 
 var credentials = require('./credentials.js');
+var fortune = require('./lib/fortune.js');
+var cartValidation = require('./lib/tourRequiresWaiver.js');
+
+let transporter = nodemailer.createTransport({
+  // эти строчки необходимы для того чтобы не было ошибки this.transporter.mailer = this
+  // но перед этим необходимо перейти в свой аккаунт гугл и разрешить доступ приложениям https://myaccount.google.com/lesssecureapps
+  // This code is nessesery to avoid Error this.transporter.mailer = this 
+  // But first you need to login in your gmail account https://myaccount.google.com/lesssecureapps and turn on access applications with low security level
+  service: 'gmail',
+  secure: false,
+  port: 25,
+  auth: {
+    user: credentials.gmail.user,
+    pass: credentials.gmail.password
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+// transporter.sendMail({
+//   from: '"John" <foo@gmail.com>',
+//   to: 'bar@gmail.com, "Vasya" <foo@gmail.com>',
+//   subject: 'It\'s third letter',
+//   text: 'Текст сообщения тут'
+// }, function(err, res){
+//   if(err){ 
+//     console.log('Невозможно отправить письмо' + err);
+//   } else {
+//     console.log('Сообщение отправлено');
+//     console.log(res);
+//   }
+// });
 
 // Установка механизма представления HTML handlebars
 var handlebars = require('express-handlebars').create({
   defaultLayout:'main',
   //этот код надо внести для того чтобы не было Error: Missing helper "section"
-  //this piece of a code is necessary to avoid Error: Missing helper "section"
+  //this code is necessary to avoid Error: Missing helper "section"
   helpers: {
     section: function(name, options){
       if(!this._sections) this._sections = {};
-      this._sections[name]=options.fn(this);
+      this._sections[name] = options.fn(this);
       return null;
     }
   } 
@@ -33,6 +66,8 @@ app.use(require('express-session')({
 }));
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser').urlencoded({ extended: true }));
+
+
 
 // Обработчик событий Flash message
 app.use(function(req, res, next){
@@ -102,6 +137,74 @@ function NewsletterSignup(){
 NewsletterSignup.prototype.save = function(cb){
   cb();
 };
+
+// mocking product database
+function Product(){
+}
+Product.find = function(conditions, fields, options, cb){
+  if(typeof conditions==='function') {
+    cb = conditions;
+    conditions = {};
+    fields = null;
+    options = {};
+  } else if(typeof fields==='function') {
+    cb = fields;
+    fields = null;
+    options = {};
+  } else if(typeof options==='function') {
+    cb = options;
+    options = {};
+  }
+  var products = [
+    {
+      name: 'Hood River Tour',
+      slug: 'hood-river',
+      category: 'tour',
+      maximumGuests: 15,
+      sku: 723,
+    },
+    {
+      name: 'Oregon Coast Tour',
+      slug: 'oregon-coast',
+      category: 'tour',
+      maximumGuests: 10,
+      sku: 446,
+    },
+    {
+      name: 'Rock Climbing in Bend',
+      slug: 'rock-climbing/bend',
+      category: 'adventure',
+      requiresWaiver: true,
+      maximumGuests: 4,
+      sku: 944,
+    }
+  ];
+  cb(null, products.filter(function(p) {
+    if(conditions.category && p.category!==conditions.category) return false;
+    if(conditions.slug && p.slug!==conditions.slug) return false;
+    if(isFinite(conditions.sku) && p.sku!==Number(conditions.sku)) return false;
+    return true;
+  }));
+};
+Product.findOne = function(conditions, fields, options, cb){
+  if(typeof conditions==='function') {
+    cb = conditions;
+    conditions = {};
+    fields = null;
+    options = {};
+  } else if(typeof fields==='function') {
+    cb = fields;
+    fields = null;
+    options = {};
+  } else if(typeof options==='function') {
+    cb = options;
+    options = {};
+  }
+  Product.find(conditions, fields, options, function(err, products){
+    cb(err, products && products.length ? products[0] : null);
+  });
+};
+
 var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
 app.post('/newsletter', function(req, res){
@@ -228,6 +331,44 @@ app.post('/contest/vacation-photo/:year/:month', function(req, res){
     res.redirect(303, '/thank-you');
   });
 });
+
+app.get('/tours/:tour', function(req, res, next){
+  Product.findOne({ category: 'tour', slug: req.params.tour }, function(err, tour){
+    if(err) return next(err);
+    if(!tour) return next();
+    res.render('tour', { tour: tour });
+  });
+});
+app.get('/adventures/:subcat/:name', function(req, res, next){
+  Product.findOne({ category: 'adventure', slug: req.params.subcat + '/' + req.params.name  }, function(err, adventure){
+    if(err) return next(err);
+    if(!adventure) return next();
+    res.render('adventure', { adventure: adventure });
+  });
+});
+
+var cartValidation = require('./lib/cartValidation.js');
+
+app.use(cartValidation.checkWaivers);
+app.use(cartValidation.checkGuestCounts);
+
+app.post('/cart/add', function(req, res, next){
+  var cart = req.session.cart || (req.session.cart = []);
+  Product.findOne({ sku: req.body.sku }, function(err, product){
+    if(err) return next(err);
+    if(!product) return next(new Error('Unknown product SKU: ' + req.body.sku));
+    cart.push({
+      product: product,
+      guests: req.body.guests || 0,
+    });
+    res.redirect(303, '/cart');
+  });
+});
+app.get('/cart', function(req, res){
+  var cart = req.session.cart || (req.session.cart = []);
+  res.render('cart', { cart: cart });
+});
+
 
 // Обобщенный обработчик 404 (промежуточное ПО)
 app.use(function(req, res) {
